5ce99aff9ba718cd7dd7955673cf03aec07558b4
wordpressjeff <me@jeffersonreal.com>
2022-05-17 16:03:08 +0100

    remove duplicate attr placeholder on input

--- parts/form.php
+++ parts/form.php
@@ -48,7 +48,6 @@ $classes     .= ' ' . $align;
                 class="bigup__form_input"
                 name="name"
                 type="text"
-                placeholder="Name (required)"
                 maxlength="100"
                 title="Name"
                 required aria-label="Name"
